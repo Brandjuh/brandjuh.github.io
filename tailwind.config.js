6c1d83ffcb7808b5f400e2ca5d45e82edf5cedb6
Brandjuh <Brandjuh@users.noreply.github.com>
2025-07-16 01:31:03 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
     './index.html',
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        'vigo-blue': '#002E5D',
+        'vigo-gray': '#8A8D8F',
+        'vigo-orange': '#F58220',
+        'vigo-red': '#E30613',
+        'vigo-purple': '#7C2851',
+        'vigo-light-blue': '#00AEEF',
+      }
+    }
+  }
   plugins: [],
 }
