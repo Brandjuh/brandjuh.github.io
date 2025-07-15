7947c00bd99885c70150269b76d681f36e796009
Brandjuh <Brandjuh@users.noreply.github.com>
2025-07-16 01:38:03 +0200

    Create VigoCard.jsx

---
+++ src/components/VigoCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VigoCard = ({ children }) => (
+  <div className="p-4 rounded-lg bg-vigo-blue text-white shadow-md">
+    {children}
+  </div>
+);
+
+export default VigoCard;
