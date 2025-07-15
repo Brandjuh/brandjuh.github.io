9ebe44b53485d1069d26dcbb7221466d12b5a3a6
Brandjuh <Brandjuh@users.noreply.github.com>
2025-07-15 20:03:49 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
-  theme: {
-    extend: {},
-  },
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './index.html',          // root entry
+    './public/index.html'    // als je nog public gebruikt
+  ],
+  theme: { extend: {} },
   plugins: [],
-};
+}
